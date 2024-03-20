f217691c328f3f4afff28e524dca88ce1d630351
Marek Bečvář <markbecvar@gmail.com>
2024-03-20 10:07:56 +0100

    More ui changes

--- PSC488/src/mainwindow.cpp
+++ PSC488/src/mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.hpp"
-#include "ui_mainwindow.h"
+#include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->monitorAmps->display(173.20);
+    ui->monitorVolts->display(178.11);
 }
 
 MainWindow::~MainWindow()
